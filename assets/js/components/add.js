41993011f128e4e029b4e9221305735ed960621d
isai <isaicatano1@gmail.com>
2026-05-27 14:17:32 -0500

    inputs

--- assets/js/components/add.js
+++ assets/js/components/add.js
@@ -5,19 +5,19 @@ const modal   = document.getElementById('mi-modal');
 const btnAbrir  = document.getElementById('btn-abrir');
 const btnCerrar = document.getElementById('btn-cerrar');
 
-// Abrir: agrega la clase "active"
+
 btnAbrir.addEventListener('click', () => {
   modal.classList.add('active');
 });
 
-// Cerrar con el botón
+
 btnCerrar.addEventListener('click', () => {
   modal.classList.remove('active');
 });
 
-// Cerrar al hacer click en el fondo oscuro
+
 modal.addEventListener('click', (e) => {
-  if (e.target === modal) {          // solo si clickeaste el fondo, no la caja
+  if (e.target === modal) {          
     modal.classList.remove('active');
   }
 });
