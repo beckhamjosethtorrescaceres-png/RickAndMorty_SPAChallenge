bd1160c81aa2fbd04f2178633d1faba2162e541a
Luis Devia <luisangeldevia679@gmail.com>
2026-05-27 15:45:48 -0500

    IMagen en location

--- assets/js/components/locationCard.js
+++ assets/js/components/locationCard.js
@@ -7,6 +7,7 @@ export function locationCard(location) {
     return `
         <article class="card">
             <div class="card-body">
+                <img src="./assets/css/img/planet.jpeg" alt="${location.name}" class="card-img">
                 <h3>${location.name}</h3>
                 <p>
                     <strong>Type:</strong>
