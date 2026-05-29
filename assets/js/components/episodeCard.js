7dfd8fff7a662c2c6c8b8739f8e8227a2041033c
andres <andrescassiani46@gmail.com>
2026-05-29 14:45:06 -0500

    listo

--- assets/js/components/episodeCard.js
+++ assets/js/components/episodeCard.js
@@ -6,21 +6,34 @@ export function episodeCard(episode) {
 
     return `
         <article class="card">
+
+            <img 
+                src="/assets/img/episodios/${episode.id}.png"
+                class="episode-img"
+                alt="${episode.name}"
+            >
+
             <div class="card-body">
+
                 <h3>${episode.name}</h3>
+
                 <p>
                     <strong>Air date:</strong>
                     ${episode.air_date}
                 </p>
+
                 <p>
                     <strong>Number character:</strong>
                     ${episode.characters.length}
                 </p>
+
                 <p>
                     <strong>Episode:</strong>
                     ${episode.episode}
                 </p>
+
             </div>
+
         </article>
     `;
 }
